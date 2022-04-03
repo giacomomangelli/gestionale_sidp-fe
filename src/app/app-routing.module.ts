043777300ef5b3c74e119b7105aa91ad0f7f2ccc
ElGnappo <lollo1.martino@gmail.com>
2022-04-03 17:23:11 +0200

    aggiunta routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'landing',
+        pathMatch: 'full'
+    },
+    {
+        path: 'landing',
+        component: LandingPageComponent
+    }
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
@@ -10,7 +11,8 @@ import { LandingPageComponent } from './pages/landing-page/landing-page.componen
     LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
